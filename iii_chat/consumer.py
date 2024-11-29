230701aaed70c130c5b0ca9218b628bb46a3de92
sharmaanchita <anchitasharma31454.com>
2024-11-29 20:55:50 +0530

    added users online

--- iii_chat/consumer.py
+++ iii_chat/consumer.py
@@ -16,17 +16,29 @@ class ChatroomConsumer(WebsocketConsumer):
             self.chatroom_name, self.channel_name
         )
         
+        # add and update online users
+        if self.user not in self.chatroom.user_online.all():
+            self.chatroom.user_online.add(self.user)
+            self.handle_online_count()
+            self.handle_online_usernames()
+        
         self.accept()
         
         
     def disconnect(self, close_code):
+        
         async_to_sync(self.channel_layer.group_discard)(
             self.chatroom_name, self.channel_name
         )
+        # remove and update online users
+        if self.user in self.chatroom.user_online.all():
+            self.chatroom.user_online.remove(self.user)
+            self.handle_online_count()
+            self.handle_online_usernames()
         
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        body = text_data_json['body']
+        body = text_data_json.get('body', '') 
         
         message = ChatMessage.objects.create(
             body = body,
@@ -41,7 +53,7 @@ class ChatroomConsumer(WebsocketConsumer):
             self.chatroom_name, event
         )
         
-    def message_handler(self, event):
+    def message_handler(self, event):        
         message_id = event['message_id']
         message = ChatMessage.objects.get(id=message_id)
         context = {
@@ -51,6 +63,45 @@ class ChatroomConsumer(WebsocketConsumer):
         }
         html = render_to_string("iii_chat/partials/chat_load.html", context=context)
         self.send(text_data=html)
+        
+    def handle_online_count(self):
+        online_count = self.chatroom.user_online.count() - 1
+        
+        event = {
+            'type': 'update_count',
+            'online_count':online_count,
+        }
+        
+        async_to_sync(self.channel_layer.group_send)(
+            self.chatroom_name, event
+        )
+        
+    def update_count(self, event):
+        online_count = event['online_count']
+        html = render_to_string("iii_chat/partials/online_count.html", { 'online_count': online_count})
+        self.send(text_data=html)
+        
+    def handle_online_usernames(self):
+        online_users = self.chatroom.user_online.all()
+        usernames = [user.username for user in online_users]
+        
+        event = {
+            'type': 'update_usernames',
+            'usernames': usernames
+        }
+        
+        async_to_sync(self.channel_layer.group_send)(
+            self.chatroom_name, event
+        )
+        
+    def update_usernames(self, event):
+        usernames = event['usernames']        
+        html = render_to_string("iii_chat/partials/online_usernames.html", {'usernames': usernames})
+        self.send(text_data=html)
+        
+        
+        
+        
         
             
 
--- iii_chat/models.py
+++ iii_chat/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class ChatGroup(models.Model):
     group_name = models.CharField(max_length=100, unique=True)
+    user_online = models.ManyToManyField(User, related_name = 'online_in_groups', blank=True)
     
     def __str__(self):
         return self.group_name
